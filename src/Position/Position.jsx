60f9738c93d3b7b4d68f6017bb675c599b1c1470
TaslimuddinAnsari <taslimuddin.ansari@madgicaltechdom.com>
2022-07-30 19:32:50 +0530

    Position

--- src/Position/Position.jsx
+++ src/Position/Position.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 // import './position-static.css';
 // import './position-relative.css';
-import './position-absolute.css';
+// import './position-absolute.css';
 // import './position-fixed.css';
-// import './position-sticky.css';
+import './position-sticky.css';
 
 const Position = () => {
   return (
     <div className='outer-block-div'>
+      <h2>Hello world</h2>
+      <h2>Hello world</h2>
       <div className='inner-block-div'>
         <div className='text-hello'>Hello world</div>
       </div>
+
+      <h2>Hello world</h2>
+      <h2>Hello world</h2>
+      <h2>Hello world</h2>
+      <h2>Hello world</h2>
+      <h2>Hello world</h2>
+      <h2>Hello world</h2>
+      <h2>Hello world</h2>
     </div>
   )
 }
